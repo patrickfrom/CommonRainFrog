27ba2476db1462dbed758881e2fd91db80a090fc
patrickfrom <patrickfromgnomedk@gmail.com>
2024-04-12 13:12:10 +0200

    Pixelated Post Processing (Removed Recreation of Framebuffer since it made issues when in fullscreen & doing alt + tab)

--- CommonRainFrog/Assets/Shaders/quad.frag
+++ CommonRainFrog/Assets/Shaders/quad.frag
@@ -6,9 +6,25 @@ in vec2 TexCoords;
 
 uniform sampler2D screenTexture;
 
+const float rt_w = 1280.0;
+const float rt_h = 720.0; 
+const float pixel_w = 5.0; 
+const float pixel_h = 5.0; 
+
 void main()
 {
-    FragColor = texture(screenTexture, TexCoords);
-    float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;
-    FragColor = vec4(average, average, average, 1.0);
+    vec3 tc = vec3(1.0, 0.0, 0.0);
+    if (TexCoords.x < (2.0-0.005))
+    {
+        float dx = pixel_w*(1./rt_w);
+        float dy = pixel_h*(1./rt_h);
+        vec2 coord = vec2(dx*floor(TexCoords.x/dx),
+        dy*floor(TexCoords.y/dy));
+        tc = texture(screenTexture, coord).rgb;
+    }
+    else if (TexCoords.x>=(2.0+0.005))
+    {
+        tc = texture(screenTexture, TexCoords).rgb;
+    }
+    FragColor = vec4(tc, 1.0);
 }  
--- CommonRainFrog/RainFrogApplication.cs
+++ CommonRainFrog/RainFrogApplication.cs
@@ -38,8 +38,8 @@ public class RainFrogApplication(int width, int height, string title) : GameWind
     private Texture2D? _stackedStoneAmbientOcclusionMap;
     private Texture2D? _stackedStoneMetallicMap;
     private Texture2D? _stackedStoneRoughnessMap;
-    private Texture2D? _stackedStoneNormalMap;  
-    
+    private Texture2D? _stackedStoneNormalMap;
+
     private Texture2D? _texturedAluminumAlbedoMap;
     private Texture2D? _texturedAluminumAmbientOcclusionMap;
     private Texture2D? _texturedAluminumMetallicMap;
@@ -81,7 +81,7 @@ public class RainFrogApplication(int width, int height, string title) : GameWind
 
 
         _quadShader = new Shader("Assets/Shaders/quad.vert", "Assets/Shaders/quad.frag");
-        
+
         _pbrShader = new Shader("Assets/Shaders/pbr.vert", "Assets/Shaders/pbr.frag");
         _pbrShader.Use();
         _pbrShader.SetInt("albedoMap", 0);
@@ -89,7 +89,7 @@ public class RainFrogApplication(int width, int height, string title) : GameWind
         _pbrShader.SetInt("metallicMap", 2);
         _pbrShader.SetInt("roughnessMap", 3);
         _pbrShader.SetInt("normalMap", 4);
-        
+
         _quad = new Quad(_quadShader);
         _cube = new Cube(_pbrShader);
 
@@ -102,7 +102,7 @@ public class RainFrogApplication(int width, int height, string title) : GameWind
         _stackedStoneMetallicMap = new Texture2D("Assets/Textures/StackedStone/Metallic.png");
         _stackedStoneRoughnessMap = new Texture2D("Assets/Textures/StackedStone/Roughness.png");
         _stackedStoneNormalMap = new Texture2D("Assets/Textures/StackedStone/Normal.png");
-        
+
         _texturedAluminumAlbedoMap = new Texture2D("Assets/Textures/TexturedAluminum/Albedo.png");
         _texturedAluminumAmbientOcclusionMap = new Texture2D("Assets/Textures/TexturedAluminum/AmbientOcclusion.png");
         _texturedAluminumMetallicMap = new Texture2D("Assets/Textures/TexturedAluminum/Metallic.png");
@@ -110,37 +110,36 @@ public class RainFrogApplication(int width, int height, string title) : GameWind
         _texturedAluminumNormalMap = new Texture2D("Assets/Textures/TexturedAluminum/Normal.png");
 
         CreatePostprocessFramebuffer();
-        
+
         SetupUniformBufferObject();
     }
 
     private void CreatePostprocessFramebuffer()
     {
-        if (_postprocessFramebuffer != 0)
-        {
-            GL.DeleteFramebuffer(_postprocessFramebuffer);
-            GL.DeleteRenderbuffer(_postprocessRenderbuffer);
-            GL.DeleteTexture(_postprocessTexture);
-        }
         _postprocessFramebuffer = GL.GenFramebuffer();
         GL.BindFramebuffer(FramebufferTarget.Framebuffer, _postprocessFramebuffer);
 
         _postprocessTexture = GL.GenTexture();
         GL.BindTexture(TextureTarget.Texture2D, _postprocessTexture);
-        
-        GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgb, ClientSize.X, ClientSize.Y, 0, PixelFormat.Rgb, PixelType.UnsignedByte, 0);
+
+        GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgb, ClientSize.X, ClientSize.Y, 0,
+            PixelFormat.Rgb, PixelType.UnsignedByte, 0);
         GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
         GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
-        
-        GL.FramebufferTexture2D(FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment0, TextureTarget.Texture2D, _postprocessTexture, 0);
+
+        GL.FramebufferTexture2D(FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment0,
+            TextureTarget.Texture2D, _postprocessTexture, 0);
 
         _postprocessRenderbuffer = GL.GenRenderbuffer();
         GL.BindRenderbuffer(RenderbufferTarget.Renderbuffer, _postprocessRenderbuffer);
-        GL.RenderbufferStorage(RenderbufferTarget.Renderbuffer, RenderbufferStorage.Depth24Stencil8, ClientSize.X, ClientSize.Y);
-        
-        GL.FramebufferRenderbuffer(FramebufferTarget.Framebuffer, FramebufferAttachment.DepthStencilAttachment, RenderbufferTarget.Renderbuffer, _postprocessRenderbuffer);
-        
+        GL.RenderbufferStorage(RenderbufferTarget.Renderbuffer, RenderbufferStorage.Depth24Stencil8, ClientSize.X,
+            ClientSize.Y);
+
+        GL.FramebufferRenderbuffer(FramebufferTarget.Framebuffer, FramebufferAttachment.DepthStencilAttachment,
+            RenderbufferTarget.Renderbuffer, _postprocessRenderbuffer);
+
         GL.BindRenderbuffer(RenderbufferTarget.Renderbuffer, 0);
+
         GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
         GL.BindTexture(TextureTarget.Texture2D, 0);
     }
@@ -148,17 +147,17 @@ public class RainFrogApplication(int width, int height, string title) : GameWind
     protected override void OnUnload()
     {
         GL.DeleteBuffer(_uboMatrices);
-        
+
         GL.DeleteFramebuffer(_postprocessFramebuffer);
         GL.DeleteRenderbuffer(_postprocessRenderbuffer);
         GL.DeleteTexture(_postprocessTexture);
-        
+
         _stackedStoneAlbedoMap!.Dispose();
         _stackedStoneAmbientOcclusionMap!.Dispose();
         _stackedStoneMetallicMap!.Dispose();
         _stackedStoneRoughnessMap!.Dispose();
         _stackedStoneNormalMap!.Dispose();
-        
+
         _texturedAluminumNormalMap!.Dispose();
         _texturedAluminumAmbientOcclusionMap!.Dispose();
         _texturedAluminumMetallicMap!.Dispose();
@@ -195,14 +194,14 @@ public class RainFrogApplication(int width, int height, string title) : GameWind
         DisplayFps(e.Time);
 
         FillUniformBufferObject();
-        
+
         GL.BindFramebuffer(FramebufferTarget.Framebuffer, _postprocessFramebuffer);
         GL.Enable(EnableCap.DepthTest);
         GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
         Render2DScene();
         RenderSkybox();
         Render3DScene();
-        
+
         GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
         GL.Disable(EnableCap.DepthTest);
         GL.Clear(ClearBufferMask.ColorBufferBit);
@@ -216,10 +215,10 @@ public class RainFrogApplication(int width, int height, string title) : GameWind
 
     protected override void OnResize(ResizeEventArgs e)
     {
+        Console.WriteLine("aa");
         GL.Viewport(0, 0, ClientSize.X, ClientSize.Y);
-        CreatePostprocessFramebuffer();
     }
-
+    
     private void Render2DScene()
     {
         GL.Disable(EnableCap.CullFace);
@@ -239,21 +238,21 @@ public class RainFrogApplication(int width, int height, string title) : GameWind
         GL.Enable(EnableCap.CullFace);
         _pbrShader!.Use();
         _pbrShader.SetVector3("cameraPosition", _camera!.Position);
-        
+
         _stackedStoneAlbedoMap!.Bind();
         _stackedStoneAmbientOcclusionMap!.Bind(1);
         _stackedStoneMetallicMap!.Bind(2);
         _stackedStoneRoughnessMap!.Bind(3);
         _stackedStoneNormalMap!.Bind(4);
         RenderSphere(new Vector3(0.0f, 3.0f, 0.0f), new Vector3(1.0f, 0.0f, 0.5f));
-        
+
         _texturedAluminumAlbedoMap!.Bind();
         _texturedAluminumAmbientOcclusionMap!.Bind(1);
         _texturedAluminumMetallicMap!.Bind(2);
         _texturedAluminumRoughnessMap!.Bind(3);
         _texturedAluminumNormalMap!.Bind(4);
         RenderSphere(new Vector3(0.0f, -3.0f, 0.0f), new Vector3(1.0f, 0.0f, 0.5f));
-        
+
         _cube!.Draw(Vector3.One, new Vector3(0.0f, 0.0f, 0.0f), 0.5f, (float)_stopwatch.Elapsed.TotalSeconds);
         _cube!.Draw(new Vector3(5.0f, 0.5f, 0.5f), new Vector3(0.0f, 1.0f, 0.0f), 0.5f);
         _cube!.Draw(new Vector3(0.0f, 2.0f, 0.5f), new Vector3(0.0f, 0.0f, 1.0f), 0.5f);
@@ -325,7 +324,7 @@ public class RainFrogApplication(int width, int height, string title) : GameWind
                     float xPosition = MathF.Cos(xSegment * 2.0f * MathF.PI) * MathF.Sin(ySegment * MathF.PI);
                     float yPosition = MathF.Cos(ySegment * MathF.PI);
                     float zPosition = MathF.Sin(xSegment * 2.0f * MathF.PI) * MathF.Sin(ySegment * MathF.PI);
-                    
+
                     positions.Add(new Vector3(xPosition, yPosition, zPosition));
                     uv.Add(new Vector2(xSegment, ySegment));
                     normals.Add(new Vector3(xPosition, yPosition, zPosition));
@@ -378,23 +377,24 @@ public class RainFrogApplication(int width, int height, string title) : GameWind
             GL.BindBuffer(BufferTarget.ArrayBuffer, _sphereVbo);
             GL.BufferData(BufferTarget.ArrayBuffer, data.Count * sizeof(float), data.ToArray(),
                 BufferUsageHint.StaticDraw);
-            
+
             GL.BindBuffer(BufferTarget.ElementArrayBuffer, _sphereEbo);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Count * sizeof(int), indices.ToArray(), BufferUsageHint.StaticDraw);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Count * sizeof(int), indices.ToArray(),
+                BufferUsageHint.StaticDraw);
             const int stride = (3 + 2 + 3) * sizeof(float);
             GL.EnableVertexAttribArray(0);
             GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, stride, 0);
-            GL.EnableVertexAttribArray(1);        
+            GL.EnableVertexAttribArray(1);
             GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, stride, 3 * sizeof(float));
             GL.EnableVertexAttribArray(2);
-            GL.VertexAttribPointer(2, 2, VertexAttribPointerType.Float, false, stride, 6 * sizeof(float));  
+            GL.VertexAttribPointer(2, 2, VertexAttribPointerType.Float, false, stride, 6 * sizeof(float));
         }
-        
+
         _pbrShader!.Use();
         Matrix4 model = Matrix4.CreateTranslation(position) * Matrix4.CreateScale(scale);
 
         _pbrShader.SetMatrix4("model", model);
-        
+
         GL.BindVertexArray(_sphereVao);
         GL.DrawElements(BeginMode.TriangleStrip, _indexCount, DrawElementsType.UnsignedInt, 0);
     }
